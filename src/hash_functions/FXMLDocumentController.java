b23bf1a37ec41c26a5e74476d4536ce7eaa2e364
Andres <aguilarandres296@gmail.com>
2016-02-09 10:18:45 -0600

    Interfaz

--- src/hash_functions/FXMLDocumentController.java
+++ src/hash_functions/FXMLDocumentController.java
@@ -1,16 +1,20 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template fileCh, choose Tools | Templates
  * and open the template in the editor.
  */
 package hash_functions;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import leerArchivo.AllLines;
+
 
 /**
  *
@@ -19,17 +23,69 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private File files;
+    private Stage stage;
+    @FXML
+    private RadioButton modul, cuadra, plega, trunca, aani, encadena, rlineal, rcuadra, rdoble;
+    int opcHash=0, opcCol;
+    
+    
+   
+    @FXML
+    private void openfile(){
+        FileChooser fileCh = new FileChooser();
+        fileCh.setTitle("Open");
+        fileCh.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("txt", "*.txt")
+        );
+        files = fileCh.showOpenDialog(stage);
+        if(files != null){
+            AllLines a = new AllLines(files.getPath());
+            System.out.println("path--"+files.getPath());
+            int lineas = a.getLines();
+        }
+        
+    }
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void opciones(){
+        if (modul.isSelected()){
+            opcHash = 1;
+        }
+        
+        if (cuadra.isSelected()){
+            opcHash = 2;
+        }
+        if (plega.isSelected()){
+            opcHash = 3;
+        }
+        if (trunca.isSelected()){
+            opcHash = 4;
+        }
+        if (aani.isSelected()){
+            opcCol = 1;
+        }
+        if (encadena.isSelected()){
+            opcCol = 2;
+        }
+        if (rlineal.isSelected()){
+            opcCol = 3;
+        }
+        if (rcuadra.isSelected()){
+            opcCol = 4;
+        }
+        if (rdoble.isSelected()){
+            opcCol = 5;
+        }
+        
+        
+        
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
