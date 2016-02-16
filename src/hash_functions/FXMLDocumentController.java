44501d931636dac2d198ebed45e3111a8784def8
Andres <aguilarandres296@gmail.com>
2016-02-16 01:07:40 -0600

    Primeros Cambios para la busqueda

--- src/hash_functions/FXMLDocumentController.java
+++ src/hash_functions/FXMLDocumentController.java
@@ -38,12 +38,12 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private RadioButton modul, cuadra, plega, trunca, aani, encadena, rlineal, rcuadra, rdoble;
     @FXML
-    private Pane panel1, panel2; 
+    private Pane panel1, panel2;
     static public int opcHash = 0, opcCol = 0;
     static int[] datos;
     static int[] datosL;
     static lineal li = new lineal();
-    static cuadratico cu =  new cuadratico();
+    static cuadratico cu = new cuadratico();
     static Arre arre = new Arre();
     static Encadena cadena = new Encadena();
     private TextField dato;
@@ -62,9 +62,9 @@ public class FXMLDocumentController implements Initializable {
             int lineas = a.getLines();
             System.out.println("lineas---" + lineas);
             datos = a.AllLines(lineas);
-            datosL = new int[datos.length+1];
+            datosL = new int[datos.length + 1];
             panel1.setDisable(false);
-            
+
         }
 
     }
@@ -74,83 +74,117 @@ public class FXMLDocumentController implements Initializable {
         d.setArray(datos);
         d.calcula();
         for (int i = 1; i < datosL.length; i++) {
-            System.out.println("id "+i +"dato: "+datosL[i]);
+            System.out.println("id " + i + "dato: " + datosL[i]);
         }
     }
-    
+
     private void cuadra() {
         Cuadrado cuadrado = new Cuadrado();
         cuadrado.setArray(datos);
         cuadrado.calcula1();
         for (int i = 1; i < datosL.length; i++) {
-            System.out.println("id "+i +"dato: "+datosL[i]);
+            System.out.println("id " + i + "dato: " + datosL[i]);
         }
     }
-    
-    private void trunca(){
+
+    private void trunca() {
         Truncamiento truncas = new Truncamiento();
         truncas.setArray(datos);
         truncas.calcular();
     }
-    
-    private void plega(){
+
+    private void plega() {
         Plegamiento plegas = new Plegamiento();
         plegas.setArray(datos);
         plegas.calcular();
     }
-    
-    private void DobleHash(){
-    DobleDireccionHash DoHash = new DobleDireccionHash();
-    DoHash.setArray(datos);
-    DoHash.DobleDirecHash(Integer.parseInt(dato.getText()));
+
+    private void DobleHash() {
+        DobleDireccionHash DoHash = new DobleDireccionHash();
+        DoHash.setArray(datos);
+        DoHash.DobleDirecHash(Integer.parseInt(dato.getText()));
     }
 
     @FXML
     private void opciones() {
-        if (aani.isSelected()) {
-            opcCol = 1;
-            arre.cleanArray(datos.length);
-        }
-        if (encadena.isSelected()) {
-            opcCol = 2;
-            cadena.cleanList(datos.length);
-        }
-        if (rlineal.isSelected()) {
-            opcCol = 3;
-            
-        }
-        if (rcuadra.isSelected()) {
-            opcCol = 4;
-        }
-        if (rdoble.isSelected()) {
-            opcCol = 5;
-            DobleHash();
-        }
+       
+
+            if (aani.isSelected()) {
+                opcCol = 1;
+                if (!panel1.isDisable()) {
+                arre.cleanArray(datos.length);
+                }else{
+                }
+            }
+            if (encadena.isSelected()) {
+                opcCol = 2;
+                if (!panel1.isDisable()) {
+                cadena.cleanList(datos.length);
+                }else {
+                    
+                }
+            }
+            if (rlineal.isSelected()) {
+                opcCol = 3;
+                if (!panel1.isDisable()) {
+                }else{
+                }
+
+            }
+            if (rcuadra.isSelected()) {
+                opcCol = 4;
+            }
+            if (rdoble.isSelected()) {
+                opcCol = 5;
+                DobleHash();
+            }
+        
 
         if (modul.isSelected()) {
             opcHash = 1;
-            modul();
-            
+            if (!panel1.isDisable()) {
+                modul();
+            } else {
+                System.out.println("Holiwis :3");
+            }
+
         }
 
         if (cuadra.isSelected()) {
             opcHash = 2;
-            cuadra();
+            if (!panel1.isDisable()) {
+                cuadra();
+            } else {
+
+            }
         }
         if (plega.isSelected()) {
             opcHash = 3;
-            plega();
+            if (!panel1.isDisable()) {
+                plega();
+            } else {
+
+            }
         }
         if (trunca.isSelected()) {
             opcHash = 4;
-            trunca();
+            if (!panel1.isDisable()) {
+                trunca();
+            } else {
+
+            }
         }
         panel1.setDisable(true);
         panel2.setDisable(false);
     }
-    
+
     @FXML
-    private void restart(){
+    private void Busqueda() {
+        
+    }
+
+    @FXML
+    private void restart() {
         panel2.setDisable(true);
         panel1.setDisable(false);
     }
